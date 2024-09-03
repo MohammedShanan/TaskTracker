c440834a9d9339af40989d26a20c3fa9d9dca157
mohammedshanan <mohammedshana12@gmail.com>
2024-09-04 02:09:15 +0300

    updated board.js

--- resources/js/board.js
+++ resources/js/board.js
@@ -1,3 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.changeName = function (element) {
+        const type = element.getAttribute("data-type");
+        const prevName = element.getAttribute(`data-${type}-name`);
+        const newName = element.textContent.trim();
+        if (newName) {
+            console.log(newName);
+            element.setAttribute(`data-${type}-name`, newName);
+        } else {
+            element.textContent = prevName;
+        }
+    };
+
+    window.newTaskPrompt = function (element) {
+        const listId = element.getAttribute("data-list-id");
+        const addTask = document.querySelector(`#${listId} #new_task`);
+        const input = document.querySelector(`#${listId} input`);
+        input.focus();
+        toggleClass(element, "show", "hide");
+        toggleClass(addTask, "hide", "show");
+    };
+
+    window.addTask = function (element) {
+        const listId = element.getAttribute("data-list-id");
+        const input = document.querySelector(`#${listId} input`);
+        const addTask = document.querySelector(`#${listId} #new_task`);
+        console.log(input.value);
+        if (input.value) {
+            const lastTask = document.querySelector(`#${listId} #new_task`);
+            const task = document.createElement("li");
+            task.classList.add("list-group-item", "task");
+            task.textContent = input.value;
+            lastTask.before(task);
+            input.value = "";
+            // addTask.style.display = "none";
+        }
+    };
+    newList();
+    newTask();
+});
+
 function newList() {
     const newListCard = document.querySelector(".new-list-card");
     const addList = document.querySelector("#new_list");
@@ -20,36 +61,59 @@ function newList() {
     newListBtn.addEventListener("click", function () {
         const input = document.querySelector('input[name="list_name"]');
         if (input.value) {
-            console.log(input.value);
+            const list = createList(input.value);
+            newListCard.before(list);
+            input.value = "";
         }
     });
 }
-function newTask() {
-    const newTask = document.querySelector(".new-task-card");
-    const addTask = document.querySelector("#new_task");
-    const newTaskBtn = document.querySelector("#add_task_btn");
-    newTask.addEventListener("click", function () {
-        addTask.style.visibility = "visible";
-    });
 
+function newTask() {
     document.addEventListener("click", function (event) {
         const target = event.target;
-        const closeCard = document.querySelector("#new_task .close");
-        if (
-            (!addTask.contains(target) && target !== newTask) ||
-            target === closeCard
-        ) {
-            addTask.style.visibility = "hidden";
-        }
-    });
-
-    newTaskBtn.addEventListener("click", function () {
-        const input = document.querySelector('input[name="task_name"]');
-        if (input.value) {
-            console.log(input.value);
+        const newTask = document.querySelector(".new-task-card.hide");
+        const addTask = document.querySelector("li.show");
+        const closeCard = document.querySelector("li.show .close");
+        if (addTask && newTask) {
+            if (
+                (!addTask.contains(target) && target !== newTask) ||
+                target === closeCard
+            ) {
+                toggleClass(addTask, "show", "hide");
+                toggleClass(newTask, "hide", "show");
+            }
         }
     });
 }
 
-newList();
-newTask();
+function createList(name) {
+    const list = document.querySelector(".list-card").cloneNode();
+    const listName = document.querySelector(".list-name").cloneNode();
+    const tasks = document.createElement("ol");
+    const newTask = document.querySelector("#new_task").cloneNode(true);
+    const addTask = document.querySelector(".new-task-card").cloneNode(true);
+    const fragment = document.createDocumentFragment();
+    const button = newTask.querySelector("button");
+    let id = 2;
+    button.setAttribute("data-list-id", `list-${id}`);
+    addTask.setAttribute("data-list-id", `list-${id}`);
+    listName.textContent = name;
+    tasks.classList.add("list-group", "border-0");
+    tasks.appendChild(newTask);
+    fragment.appendChild(listName);
+    fragment.appendChild(tasks);
+    fragment.appendChild(addTask);
+    list.appendChild(fragment);
+    list.setAttribute("id", `list-${id}`);
+    return list;
+}
+
+function toggleClass(element, class1, class2) {
+    if (element.classList.contains(class1)) {
+        element.classList.remove(class1);
+        element.classList.add(class2);
+    } else {
+        element.classList.remove(class2);
+        element.classList.add(class1);
+    }
+}
--- resources/views/board.blade.php
+++ resources/views/board.blade.php
@@ -3,27 +3,25 @@
 @section('content')
     <main class="board-main-content bg4">
         <div class="board-info bg-primary-subtle">
-            <span contenteditable="true" class="board-name">Board name</span>
+            <span contenteditable="true" class="board-name" data-board-name="Tekken 7" data-type="board" onblur="changeName(this)">Tekken 7</span>
             <button class="btn btn-danger">Delete</button>
         </div>
         <section class="board-content">
-            <div class="list-card card bg-body-tertiary"> 
-                <ol class="list-group">
-                    <li>
-                        <div contenteditable="true" class="card-header list-name" >
+            <div class="list-card card bg-body-tertiary" id="list-1">
+                <div contenteditable="true" class="card-header list-name" data-list-name="Featured" data-type="list" onblur="changeName(this)">
                             Featured
-                        </div>
-                    </li>
-                    <li class="list-group-item task">task</li>
-                    <li class="list-group-item new-task-card">
-                        Add a task
-                        <div class="add-card" id='new_task'>
-                            <input class="form-control" type="text" name="task_name" placeholder="Enter list name...">
-                            <button id="add_task_btn" class="btn btn-primary mt-3">Add task</button>
-                            <i class="fa-solid fa-xmark close"></i>
-                        </div>
+                </div> 
+                <ol class="list-group border-0" >
+                    {{-- <li class="list-group-item task">test</li> --}}
+                    <li class="hide list-group-item" id='new_task'>
+                            <div class="add-card"> 
+                                <input class="form-control" type="text" name="task_name" placeholder="Enter list name...">
+                                <button class="btn btn-primary mt-3" data-list-id="list-1" onclick="addTask(this)">Add task</button>
+                                <i class="fa-solid fa-xmark close"></i>
+                            </div>
                     </li>
                 </ol>
+                <div class="new-task-card show" data-list-id="list-1" onclick="newTaskPrompt(this)">Add a task</div>
             </div>
             <div class="new-list-card card"> 
                 Add another list
